a7baf8505f8031bed7d5c8809a1b7634da9b2bb1
Lyana rodrigues tamashiro <lyanatamashiro8@gmail.com>
2024-07-05 10:34:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+import Info from "./components/Info";
+import Services from "./components/Serviços";
+function App() {
+  return (
+    <div className="App">
+       <Header />
+       <Hero />
+       <Info />
+       <Services />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Serviços.jsx
@@ -0,0 +1,16 @@
+
+
+
+export default function Service (){
+    return(
+        <>
+        <main className="">
+            <div className="min-h-screen w-full block border-2"></div>
+            <div className="h-auto">
+                <h1>Gain more insight into how people user your</h1>
+                <p>With our integrated CRM, project management, collaboration and i</p>
+            </div>
+        </main>
+        </>
+    )
+}
